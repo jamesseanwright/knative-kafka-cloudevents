a307bf9271294481f4897b8f7911b19eeece3796
James Wright <hi@james.engineering>
2023-02-18 11:34:33 +0000

    Adds WIP Kafka/CloudEvents producer

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/jamesseanwright/knative-kafka-cloudevents
+
+go 1.18
+
+require (
+	github.com/cloudevents/sdk-go/v2 v2.12.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.38 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
---
+++ internal/producer.go
@@ -0,0 +1,79 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/google/uuid"
+	"github.com/segmentio/kafka-go"
+	"golang.org/x/sync/errgroup"
+)
+
+const interval = 1 * time.Second
+
+type KafkaConn interface {
+	WriteMessages(...kafka.Message) (int, error)
+}
+
+type Producer struct {
+	conn KafkaConn
+}
+
+func NewProducer(conn KafkaConn) Producer {
+	return Producer{conn}
+}
+
+func (p Producer) Run(ctx context.Context) error {
+	ticker := time.NewTicker(interval)
+	done := ctx.Done()
+	g, _ := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		for {
+			select {
+			case <-done:
+				return nil
+
+			case <-ticker.C:
+				if err := p.writeMessage(); err != nil {
+					return err
+				}
+			}
+		}
+	})
+
+	return g.Wait()
+}
+
+func (p Producer) writeMessage() error {
+	id, err := uuid.NewRandom()
+
+	if err != nil {
+		return fmt.Errorf("cloudevent id generation: %w", err)
+	}
+
+	event := cloudevents.NewEvent(cloudevents.VersionV1)
+	event.SetID(id.String())
+	event.SetSource("knative-kafka-cloudevents/producer")
+	event.SetType("com.example.testevent.produced")
+
+	if err := event.SetData("text/plain", "test event!"); err != nil {
+		return fmt.Errorf("cloudevent set data: %w", err)
+	}
+
+	n, err := p.conn.WriteMessages(kafka.Message{
+		Value: event.Data(),
+	})
+
+	if err != nil {
+		return fmt.Errorf("kafka write: %w", err)
+	}
+
+	if n != 1 {
+		return fmt.Errorf("kafka write: incorrect number of messages published (want: 1, got: %d)", n)
+	}
+
+	return nil
+}
